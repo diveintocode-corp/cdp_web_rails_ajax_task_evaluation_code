d4bb2952ae7239b15c155e07277c5fe8b555d629
keikamiguchi <kei.kamiguchi517@gmail.com>
2022-03-02 12:04:40 +0900

    [update]国際化対応

--- system/favorites_spec.rb
+++ system/favorites_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
-RSpec.describe "お気に入り機能" do
+RSpec.describe "Favorite function" do
   let!(:user) { FactoryBot.create(:user) }
   let!(:second_user) { FactoryBot.create(:second_user) }
   let!(:third_user) { FactoryBot.create(:third_user) }
@@ -8,30 +8,30 @@ RSpec.describe "お気に入り機能" do
   let!(:third_blog) { FactoryBot.create(:third_blog, user_id: third_user.id) }
   let!(:favorite) { Favorite.create(user_id: user.id, blog_id: second_blog.id) }
 
-  describe '評価項目' do
+  describe 'Evaluation Items' do
     before do
       visit new_user_session_path
       fill_in "Email", with: user.email
       fill_in "Password", with: user.password
       click_on "Log in"
     end
-    it 'お気に入り登録と解除するリンクにあらかじめ設定されているHTMLのclass属性を削除しないこと' do
+    it 'Do not remove the HTML class attribute that is pre-set for the link to add and unsubscribe favorites.' do
       expect(page).to have_css ".delete-#{second_blog.id}"
       expect(page).to have_css ".post-#{third_blog.id}"
     end
-    it 'スターアイコンをクリックした際、お気に入りに登録できること' do
+    it 'When you click on the star icon, you can add it to your favorites.' do
       expect(Favorite.all.count).to eq 1
       find(".post-#{third_blog.id}").click
       sleep 1
       expect(Favorite.all.count).to eq 2
     end
-    it 'スターアイコンをクリックした際、お気に入りを解除できること' do
+    it 'When clicking on the star icon, you can remove it from your favorites.' do
       expect(Favorite.all.count).to eq 1
       find(".delete-#{second_blog.id}").click
       sleep 1
       expect(Favorite.all.count).to eq 0
     end
-    it '星マークの横にある数字が正しく増減すること' do
+    it 'The number next to the star icon will increase or decrease correctly.' do
       click_on "Logout"
       fill_in "Email", with: third_user.email
       fill_in "Password", with: third_user.password
